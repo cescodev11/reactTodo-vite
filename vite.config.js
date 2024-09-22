d37edb08ed62bbcf1f61a92bef5bdcf910fb97ae
Carlos Escobar <carlosWebDev11@gmail.com>
2024-09-21 19:08:42 -0700

    vite_config

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/reactTodo-vite/", // Replace with your repository name
+  base: "/reactTodo-vite/", // Make sure this matches your repository name
 });
